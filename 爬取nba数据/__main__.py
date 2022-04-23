6954b74d92601d0e6dfa8cdb59ee452ff1b46fe0
ojcsdream <99775804+ojcsdream@users.noreply.github.com>
2022-04-23 16:53:41 +0800

    添加退出按钮3

--- 爬取nba数据/__main__.py
+++ 爬取nba数据/__main__.py
@@ -1,19 +1,19 @@
 import collect_group_list
 import collect_nba_player_goals
 
-
 print("——————————欢迎来到nba数据爬取工具————————")
 print("作者github主页：https://github.com/ojcsdream/")
 while True:
-    choice = input("输入1：爬取nba排名数据\n输入2：爬取nba球员得分排名")
+    choice = input("输入1：爬取nba排名数据\n输入2：爬取nba球员得分排名\n输入3：退出程序")
     if choice in ["1", "2"]:
         if choice == "1":
             collect_group_list.main()
             break
-        else:
+        elif choice == "2":
             collect_nba_player_goals.main()
             break
-
+        else:
+            break
     else:
         print("输入错误，请输入1或2")
 
