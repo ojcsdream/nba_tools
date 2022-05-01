95fde2328509d5275e3c7ffa93923f6b774fb046
ojcsdream <99775804+ojcsdream@users.noreply.github.com>
2022-05-01 16:02:59 +0800

    重要！！！第三个功能正式上线

--- 爬取nba数据/main.py
+++ 爬取nba数据/main.py
@@ -1,17 +1,20 @@
 import collect_group_list
 import collect_nba_player_goals
+import collect_today_match
 
 print("——————————欢迎来到nba数据爬取工具————————")
 print("作者github主页：https://github.com/ojcsdream/")
 while True:
-    choice = input("输入1：爬取nba排名数据\n输入2：爬取nba球员得分排名\n输入3：退出程序")
-    if choice in ["1", "2", "-1"]:
+    choice = input("输入1：爬取nba排名数据\n输入2：爬取nba球员得分排名\n输入3：获取比赛数据\n输入-1：退出程序")
+    if choice in ["1", "2", "3", "-1"]:
         if choice == "1":
             collect_group_list.main()
             break
         elif choice == "2":
             collect_nba_player_goals.main()
             break
+        elif choice == "3":
+            collect_today_match.main()
         else:
             break
     else:
